e40cd2806a34f82bfb25a74a492274d891cb5056
Max Desiatov <max@desiatov.com>
2020-12-06 20:05:47 +0000

    Initial Commit

---
+++ Shared/GravityApp.swift
@@ -0,0 +1,17 @@
+//
+//  GravityApp.swift
+//  Shared
+//
+//  Created by Max Desiatov on 06/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct GravityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
